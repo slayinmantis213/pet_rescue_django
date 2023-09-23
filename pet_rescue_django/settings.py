e22ac112588a51fd6fa39d19e264ae09da557cbb
Joshua Bliek <jbliek213@yahoo.com>
2023-09-23 03:29:53 -0700

    final push

--- pet_rescue_django/settings.py
+++ pet_rescue_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-(f=z$iy8&wve6oiqrq0pjz(v=@&sf+^azj*o2$4ejt-!3(4gq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['73.170.167.225', 'localhost', '127.0.0.1']
 
 
 # Application definition
